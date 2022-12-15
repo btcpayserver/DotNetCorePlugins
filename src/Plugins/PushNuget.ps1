ff252336e859aa048fbbb6f04f10012fef7de4e6
nicolas.dorier <nicolas.dorier@gmail.com>
2022-12-15 12:44:30 +0900

    Fix PushNuget.ps1

--- src/Plugins/PushNuget.ps1
+++ src/Plugins/PushNuget.ps1
@@ -1,7 +1,7 @@
 rm "bin\release\" -Recurse -Force
 dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
-$package=(ls .\bin\Release\*.nupkg).FullName
+$package=(ls ..\..\.build\bin\McMaster.NETCore.Plugins\Release\*.nupkg).FullName
 dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
-$ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '.*(\d+\.\d+\.\d+)\.nupkg','$1')
+$ver = ((ls ..\..\.build\bin\McMaster.NETCore.Plugins\Release\*.nupkg)[0].Name -replace '.*(\d+\.\d+\.\d+)\.nupkg','$1')
 git tag -a "btcpay/v$ver" -m "btcpay/v$ver"
 git push origin "btcpay/v$ver"
